0bd31b8915c767b7fe8366cb8f88d458ee042def
ingridt123 <ingridszenga@gmail.com>
2020-12-21 18:31:46 +0800

    Add responsive nav bar

--- src/components/exercises-section.js
+++ src/components/exercises-section.js
@@ -14,11 +14,18 @@ const Exercise = ({ title, gif, description }) => (
 )
 
 // TODO: add links for headers?
+// TODO: global state?
 export default function ExercisesSection({ headerText, exercises }) {
+    // const [mobile, setMobile] = useState(window.innerWidth < MOBILE_WIDTH);
+    // useEffect(() => {
+    //     window.addEventListener('resize', () => {setMobile(window.innerWidth < MOBILE_WIDTH);});
+    // }, [])
+    // ${mobile ? styles.exerciseContainer_mobile : ""}
+
     return (
         <div>
             <h2>{headerText}</h2>
-            {exercises.map( (e, index)  => {
+            {exercises.map( e  => {
                 if (e.title !== null) {
                     return <Exercise title={e.title} gif={e.gifUrl} description={e.description} key={e.title} />
                 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
-import { DropdownButton, Dropdown } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react'
+import { DropdownButton, Dropdown } from 'react-bootstrap'
 import { useStaticQuery, Link, graphql } from "gatsby"
+import { FaBars } from "@react-icons/all-files/fa/FaBars"
+import { FaTimes } from "@react-icons/all-files/fa/FaTimes"
+import { FaCaretDown } from "@react-icons/all-files/fa/FaCaretDown"
 
 import styles from "./layout.module.css"
 
@@ -8,12 +11,17 @@ import styles from "./layout.module.css"
 // title
 // navigation bar with dropdowns (general, upper body, lower body)
 
-// TODO: change nav bar for responsive
 // TODO: add caret?
 // TODO: add authentication
 // TODO: add change size
 // TODO: add change language
+// TODO: add top button?
 export default function Layout({ children }) {
+    // const [mobile, setMobile] = useState(window.innerWidth < MOBILE_WIDTH);
+    // useEffect(() => {
+    //     window.addEventListener('resize', () => {setMobile(window.innerWidth < MOBILE_WIDTH);});
+    // }, [])
+
     const data = useStaticQuery(
         graphql`
             query {
@@ -37,6 +45,30 @@ export default function Layout({ children }) {
         `
     )
 
+    const dropdownTitles = ["general", "upper", "lower"]
+    const generalPages = [
+        {
+            title: "Terminology",
+            href: "/terminology",
+        },
+        {
+            title: "Stretches",
+            href: "/stretches",
+        },
+        {
+            title: "Activation",
+            href: "/activation",
+        },
+        {
+            title: "Warm Up",
+            href: "/warm-up",
+        },
+        {
+            title: "Cool Down",
+            href: "/cool-down",
+        },
+    ]
+
     // const [dropdown, setDropdown] = useState({
     //     "general": false, 
     //     "upper": false, 
@@ -48,7 +80,6 @@ export default function Layout({ children }) {
     // setDropdown(newState);
 
     const [dropdown, setDropdown] = useState(null);
-    const dropdownTitles = ["general", "upper", "lower"]
 
     function hoverOn(title) {
         setDropdown(title);
@@ -58,22 +89,44 @@ export default function Layout({ children }) {
         setDropdown(null);
     }
 
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [mobileMenuDropdown, setMobileMenuDropdown] = useState(null)
+
+    function handleMobileMenu() {
+        setMobileMenuDropdown(null);
+        setMobileMenu(!mobileMenu);
+    }
+
+    function closeMobileMenu() {
+        setMobileMenuDropdown(null);
+        setMobileMenu(false);
+    }
+
+    function handleMobileMenuDropdown(title) {
+        if (mobileMenuDropdown === title) {
+            setMobileMenuDropdown(null);
+        } else {
+            setMobileMenuDropdown(title);
+        }
+    }
+
     return (
         <div id={styles.container}>
             <header id={styles.header}>
                 <Link to="/" id={styles.headerLink}>
                     <h3 id={styles.headerHeading}>{data.site.siteMetadata.title}</h3>
                 </Link>
+                {/* DESKTOP VERSION */}
                 <ul id={styles.dropdowns}>
                     <DropdownButton title="General" className={styles.dropdown}
                                     onMouseEnter={hoverOn.bind(this, dropdownTitles[0])}
                                     onMouseLeave={hoverOff.bind(this)}
                                     show={dropdown === dropdownTitles[0]}>
-                        <Dropdown.Item href="/terminology" className={styles.dropdownItem}>Terminology</Dropdown.Item>
-                        <Dropdown.Item href="/stretches">Stretches</Dropdown.Item>
-                        <Dropdown.Item href="/activation">Activation</Dropdown.Item>
-                        <Dropdown.Item href="/warm-up">Warm Up</Dropdown.Item>
-                        <Dropdown.Item href="/cool-down">Cool Down</Dropdown.Item>
+                        {generalPages.map( gp => {
+                            return <Dropdown.Item href={gp.href} key={gp.title}>
+                                        {gp.title}
+                                    </Dropdown.Item>
+                        })}
                     </DropdownButton>
                     <DropdownButton title="Upper Body" className={styles.dropdown}
                                     onMouseEnter={hoverOn.bind(this, dropdownTitles[1])}
@@ -81,7 +134,9 @@ export default function Layout({ children }) {
                                     show={dropdown === dropdownTitles[1]}>
                         {data.allBodyPart.edges.map( bp => {
                             if (bp.node.type === "upper") {
-                                return <Dropdown.Item href={"/" + bp.node.id} key={bp.node.id}>{bp.node.fields.title}</Dropdown.Item>
+                                return <Dropdown.Item href={"/" + bp.node.id} key={bp.node.id}>
+                                            {bp.node.fields.title}
+                                        </Dropdown.Item>
                             }
                             return <></>
                         })}
@@ -92,13 +147,65 @@ export default function Layout({ children }) {
                                     show={dropdown === dropdownTitles[2]}>
                         {data.allBodyPart.edges.map( bp => {
                             if (bp.node.type === "lower") {
-                                return <Dropdown.Item href={"/" + bp.node.id} key={bp.node.id}>{bp.node.fields.title}</Dropdown.Item>
+                                return <Dropdown.Item href={"/" + bp.node.id} key={bp.node.id}>
+                                            {bp.node.fields.title}
+                                        </Dropdown.Item>
                             }
                             return <></>
                         })}
                     </DropdownButton>
                 </ul>
+                {/* MOBILE VERSION */}
+                <div id={styles.mobileMenu_icon} onClick={handleMobileMenu.bind(this)}>
+                    {!mobileMenu ? <FaBars /> : <FaTimes />}
+                </div>
             </header>
+            {mobileMenu ? 
+                <div id={styles.mobileMenu}>
+                    <ul id={styles.dropdownsMobile}>
+                        <li>
+                            <h3 className={styles.dropdownMobile_header} onClick={handleMobileMenuDropdown.bind(this, dropdownTitles[0])}>
+                                General <FaCaretDown />
+                            </h3>
+                                {generalPages.map( gp => {
+                                        return <a href={gp.href} key={gp.title} 
+                                                className={mobileMenuDropdown === dropdownTitles[0] ? styles.dropdownMobile : styles.dropdownMobile_hide}
+                                                onClick={closeMobileMenu.bind(this)}>
+                                                    {gp.title}
+                                                </a>
+                                })}
+                        </li>
+                        <li>
+                            <h3 className={styles.dropdownMobile_header} onClick={handleMobileMenuDropdown.bind(this, dropdownTitles[1])}>
+                                Upper Body <FaCaretDown />
+                            </h3>
+                                {data.allBodyPart.edges.map( bp => {
+                                    if (bp.node.type === "upper") {
+                                        return <a href={"/" + bp.node.id} key={bp.node.id}
+                                                className={mobileMenuDropdown === dropdownTitles[1] ? styles.dropdownMobile : styles.dropdownMobile_hide}>
+                                                    {bp.node.fields.title}
+                                                </a>
+                                    }
+                                    return <></>
+                                })}
+                        </li>
+                        <li>
+                            <h3 className={styles.dropdownMobile_header} onClick={handleMobileMenuDropdown.bind(this, dropdownTitles[2])}>
+                                Lower Body <FaCaretDown />
+                            </h3>
+                                {data.allBodyPart.edges.map( bp => {
+                                    if (bp.node.type === "lower") {
+                                        return <a href={"/" + bp.node.id} key={bp.node.id}
+                                                className={mobileMenuDropdown === dropdownTitles[2] ? styles.dropdownMobile : styles.dropdownMobile_hide}>
+                                                    {bp.node.fields.title}
+                                                </a>
+                                    }
+                                    return <></>
+                                })}
+                        </li>
+                    </ul>
+                </div>
+            : <></>}
             {children}
         </div>
     )
